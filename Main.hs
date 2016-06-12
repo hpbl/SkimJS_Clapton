6151cbb3d00a87e6e1c7b48f320aae64ceaa8e56
Hilton Pintor <hpbl@cin.ufpe.br>
2016-06-12 15:09:07 -0300

    Mudancas feitas por nicola

--- Main.hs
+++ Main.hs
@@ -11,9 +11,19 @@ import Value
 --
 
 evalExpr :: StateT -> Expression -> StateTransformer Value
+evalExpr env NullLit = return Nil
+evalExpr env (StringLit str) = return (String str)
 evalExpr env (VarRef (Id id)) = stateLookup env id
 evalExpr env (IntLit int) = return $ Int int
 evalExpr env (BoolLit bool) = return $ Bool bool
+
+evalExpr env (ArrayLit list) = do
+    case list of
+        [] -> return (List [])
+        (x:xs) -> do
+            hd <- evalExpr env x
+            (List tl) <- evalExpr env (ArrayLit xs)
+            return (List ([hd]++tl))
 evalExpr env (InfixExpr op expr1 expr2) = do
     v1 <- evalExpr env expr1
     v2 <- evalExpr env expr2
@@ -22,56 +32,111 @@ evalExpr env (AssignExpr OpAssign (LVar var) expr) = do
     stateLookup env var -- crashes if the variable doesn't exist
     e <- evalExpr env expr
     setVar var e
+evalExpr env (DotRef exp (Id id)) = do
+    l <- evalExpr env (exp)
+    case l of
+        (List (x:xs)) -> do
+            case id of
+                "len" -> return (myLength (l) (Int 0))
+                "head" -> return x
+                "tail" -> return (List xs)
+        (List []) -> do
+            case id of
+                "len" -> return (Int 0)
+                "head" -> error $ "Lista vazia"
+                "tail" -> error $ "Lista vazia"
+        _ -> do 
+            error $ "error"
+evalExpr env (CallExpr dot lista) = do
+    case dot of 
+        (DotRef exp (Id id)) -> do
+            l <- evalExpr env (exp)
+            case l of
+                (List l) -> do
+                    case id of
+                        "concat" -> (myConcat env (l) (lista))
+                _ -> do
+                    error $ "error"
+        _ -> return Nil
+
+
+myLength :: Value -> Value -> Value
+myLength (List []) (Int num) = (Int num)
+myLength  (List (l:ls)) (Int num) = myLength  (List ls) (Int (num + 1) )
+
+
+myConcat :: StateT ->[Value]-> [Expression] -> StateTransformer Value
+myConcat env (list) ([]) = return (List list)
+myConcat env (list) (x:xs) = do
+                vm <- evalExpr env x;
+                case vm of
+                    (List e) -> myConcat env (list ++ e) (xs)
+                    x -> myConcat env (list ++ [x]) xs
+
+
+--auxEqual :: StateT -> [Value] -> [Expression] -> Value
+--auxEqual env [] [] = (Bool True)
+--auxEqual env list (x:xs) = do
+--            vm <- evalExpr env x
+--            myEqual (list) (vm)
+
+--myEqual :: [Value]-> [Value] -> Value
+--myEqual  [] [] = (Bool True)
+--myEqual  (_) ([]) = (Bool False)
+--myEqual  ([]) (_) = (Bool False)
+--myEqual  (y:ys) (x:xs) = (x == y) && myEqual (ys) (xs)
+
 
 evalStmt :: StateT -> Statement -> StateTransformer Value
 evalStmt env EmptyStmt                = return Nil
-
 evalStmt env (VarDeclStmt [])         = return Nil
 evalStmt env (VarDeclStmt (decl:ds))  =
     varDecl env decl >> evalStmt env (VarDeclStmt ds)
 
 evalStmt env (ExprStmt expr)          = evalExpr env expr
-
 --inicio da edicao
 
 --Bloco de comandos
 evalStmt env (BlockStmt [])           = return Nil
-evalStmt env (BlockStmt [stmt])       = evalStmt env stmt 
-evalStmt env (BlockStmt (st:stmts))   = evalStmt env st >> evalStmt env (BlockStmt stmts)
+--evalStmt env (BlockStmt ((BreakStmt st):stmts)) = return Break
+--evalStmt env (BlockStmt [stmt])       = evalStmt env stmt
+evalStmt env (BlockStmt (st:stmts))   = do
+    evaluedStmt <- evalStmt env st
+    case evaluedStmt of
+        Break -> return Break
+        _ -> evalStmt env (BlockStmt stmts)
 
---break
---evalStmt env (BreakStmt id)           = return True 
+evalStmt env (BreakStmt st) = return Break
 
 
 --If simples
 evalStmt env (IfSingleStmt expr stmt) = do
     x <- evalExpr env expr
     if (evalBoolean x)
-        then evalStmt env stmt 
+        then evalStmt env stmt
         else return Nil
+
+
 --If com else
 evalStmt env (IfStmt expr stmt stmt2) = do
     x <- evalExpr env expr
     if (evalBoolean x)
-        then evalStmt env stmt 
+        then evalStmt env stmt
         else evalStmt env stmt2
+
+
 --While
 evalStmt env (WhileStmt expr stmt) = do
     x <- evalExpr env expr
     if (evalBoolean x)
-        then do 
+        then do
             y <- evalStmt env stmt
             if(isBreak(y))
-                then return Nil
-                else evalStmt env (WhileStmt expr stmt)
+               then return Nil
+               else evalStmt env (WhileStmt expr stmt)
         else return Nil
---evalStmt env (WhileStmt expr stmt) = do
---    x <- evalExpr env expr
---    if (evalBoolean x)
---        then if(isBreak (evalStmt env stmt)) 
---                 then return Nil
---                 else evalStmt env (WhileStmt expr stmt)
---        else return Nil
+
+
 --Do-While
 evalStmt env (DoWhileStmt stmt expr)  = do
     evalStmt env stmt
@@ -79,6 +144,8 @@ evalStmt env (DoWhileStmt stmt expr)  = do
     if (evalBoolean x)
         then evalStmt env (WhileStmt expr stmt)
         else return Nil
+
+
 --For
 evalStmt env (ForStmt init test inc stmt) = do
     case init of
@@ -88,33 +155,67 @@ evalStmt env (ForStmt init test inc stmt) = do
     case test of
         Nothing -> do
             case inc of
-                Nothing -> evalStmt env stmt >> evalStmt env (ForStmt NoInit test inc stmt)
-                Just incM -> evalStmt env stmt >> evalExpr env incM >> evalStmt env (ForStmt NoInit test inc stmt)
+                Nothing -> do 
+                    evaluedStmt <- evalStmt env stmt
+                    if(isBreak evaluedStmt)
+                        then return Nil                
+                        else evalStmt env (ForStmt NoInit test inc stmt)
+                Just incM -> do
+                    evaluedStmt <- evalStmt env stmt
+                    if(isBreak evaluedStmt)
+                        then return Nil
+                        else evalExpr env incM >> evalStmt env (ForStmt NoInit test inc stmt)
         Just testM -> do
             evaluedTest <- evalExpr env testM
             if (evalBoolean evaluedTest)
                 then case inc of
-                    Nothing -> evalStmt env stmt >> evalStmt env (ForStmt NoInit test inc stmt)
-                    Just incM -> evalStmt env stmt >> evalExpr env incM >> evalStmt env (ForStmt NoInit test inc stmt)
+                    Nothing -> do
+                        evaluedStmt <- evalStmt env stmt
+                        if(isBreak evaluedStmt)
+                            then return Nil
+                            else evalStmt env (ForStmt NoInit test inc stmt)
+                    Just incM -> do 
+                        evaluedStmt <- evalStmt env stmt
+                        if(isBreak evaluedStmt)
+                            then return Nil
+                            else evalExpr env incM >> evalStmt env (ForStmt NoInit test inc stmt)
                 else return Nil
---function
---evalStmt env (FunctionStmt nome args corpo) = do
---  evalStmt env nome >> evalStmt env args >> evalStmt env corpo
+
 
 evalBoolean :: Value -> Bool
 evalBoolean (Bool b) = b
 
---isBreak :: StateTransformer Value -> Bool
---isBreak (_ Nil) = True
---isBreak (_ _)  = False
-isBreak ::Value -> Bool
-isBreak (Nil) = True
-isBreak _ = False
+takeId :: Expression -> [Char]
+takeId (DotRef a (Id id)) = id
 
 -- Do not touch this one :)
 evaluate :: StateT -> [Statement] -> StateTransformer Value
 evaluate env [] = return Nil
 evaluate env stmts = foldl1 (>>) $ map (evalStmt env) stmts
+
+
+
+isDotRef :: Expression -> Bool
+isDotRef (DotRef (exp) (Id id)) = True
+isDotRef _ = False
+
+
+
+
+--compList :: Value -> Value -> Bool
+--compList (List []) (List []) = True
+--compList (List _) (List []) = False
+--compList  (List []) (List _) = False
+--compList (List (l:ls)) (List (a:as)) 
+--    | (a == l) = compList (ls) (as)
+--    | otherwise = False
+
+
+
+isBreak :: Value -> Bool
+isBreak (Break) = True
+isBreak _ = False
+
 
 --
 -- Operators
--- Main.js
+++ Main.js
@@ -1,7 +1,8 @@
-var x = 1;
-
-while (x < 10) {
-	x = x + 1;
-	break;
-} 
-x;
+var list = [];
+var list2 = [];
+for(var i = 0; i < 10; i = i + 1){
+	if(i == 5)break;
+	list = list.concat([i]);
+	list2 = list2.concat([i+1]);
+}
+list.compare(list2);
